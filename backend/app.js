a0b878faa24e380f3aebb732f35750468001e199
Gary <glswim@gmail.com>
2024-10-29 10:56:42 +0800

    Changed from require to import express

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,15 @@
 // server/app.js
-const express = require('express');
+import express, { json } from 'express';
 const app = express();
 const PORT = process.env.PORT || 5000;
-const routes = require("./routes/userRoutes");
-const cors = require("cors");
+import routes from "./routes/userRoutes";
+import cors from "cors";
 const corsOption = {
   origin: ["http://localhost:3000"],
 };
 app.use(cors(corsOption));
 
-app.use(express.json());
+app.use(json());
 app.use("/", routes);
 
 console.log("post something");
